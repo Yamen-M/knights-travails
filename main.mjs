bece48395f2ac59df80ae777960751f85f874d37
Yamen Morcel <77121917+Yamen-M@users.noreply.github.com>
2025-08-02 10:13:20 +0300

    fixed file import typo and cleaned output

--- main.mjs
+++ main.mjs
@@ -1,7 +1,16 @@
-import knightMoves from "./knightMoves.js";
+import knightMoves from "./knightMoves.mjs";
 
+console.log("\nFrom [0,0] to [1,2]:");
 knightMoves([0, 0], [1, 2]);
+
+console.log("\nFrom [0,0] to [3,3]:");
 knightMoves([0, 0], [3, 3]);
+
+console.log("\nFrom [3,3] to [0,0]:");
 knightMoves([3, 3], [0, 0]);
+
+console.log("\nFrom [0,0] to [7,7]:");
 knightMoves([0, 0], [7, 7]);
+
+console.log("\nFrom [3,3] to [4,3]:");
 knightMoves([3, 3], [4, 3]);
